2b06311cb9ccd8f73ed8f5ae89869c40469a954a
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-05-25 22:26:28 +0500

    home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ['./templates/**/*.html'],
   theme: {
     extend: {},
+    fontSize: {
+      'rg': '16px',
+      'sm': '18px',
+      'md': '22px',
+      'lg': '36px',
+      'xl': '42px',
+      '2xl': '56px',
+      '3xl': '72px'
+    },
+    fontFamily: {
+      'tt-regular': 'tt-regular',
+      'tt-medium': 'tt-medium',
+      'tt-bold': 'tt-bold',
+    },
   },
   plugins: [],
 }
